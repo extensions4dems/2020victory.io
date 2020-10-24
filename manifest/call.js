0cc905604fa5e87a37d37d3518e03220af23a09d
extensions4dems <73376553+extensions4dems@users.noreply.github.com>
2020-10-24 12:29:45 -0700

    Update call.js

--- manifest/call.js
+++ manifest/call.js
@@ -1,11 +1,11 @@
 'use strict';
 
-// look for a term code.
+// looks for a term code(s).
 // needs to run after native code executes so just keep checking after each click
-//TODO: ends up firing twice per click, probably need to filter on source to just run once
+//TODO: ends up firing twice per click, probably needs to filter on source to just run once
 document.addEventListener("click",
     function() {
-        // wait a bit after the click so the native code has a change to run
+        // wait a bit after the click so the native code has a chance to run
     	setTimeout( function() {
             // term codes are in the unique tag, "kbd"
     		var kbdEls = document.getElementsByTagName("kbd");
